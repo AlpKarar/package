e8a5d1dddb08a1b83837471733d4ed1e05833101
Silicon Light <siliconlight47@gmail.com>
2023-09-05 10:25:02 +0300

    rebuild-1:main

--- api-trial/main.go
+++ api-trial/main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os"
 )
 
-const PORT string = ":9090"
-
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		d, _ := ioutil.ReadAll(r.Body)
-		//fmt.Println("Data: ", string(d))
-		fmt.Fprintf(os.Stdout, "Hello %s\n", d)
-
-		rw.Write([]byte("HOME PAGE"))
-	})
-
-	fmt.Println("Server started running on port:", PORT[1:])
-	http.ListenAndServe(PORT, nil)
+	fmt.Println("HOME")
 }
